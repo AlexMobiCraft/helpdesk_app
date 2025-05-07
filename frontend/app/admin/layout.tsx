8a4c550a112d57bb8bc4b9749435fa1228913946
AlexMobiCraft <alex.mobicraft@gmail.com>
2025-05-07 20:12:45 +0200

    feat: добавлен базовый шаблон для админ-панели с поддержкой i18n

--- frontend/app/admin/layout.tsx
+++ frontend/app/admin/layout.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import React from 'react';
-import { AppBar, Container, Button } from '@mui/material';
+import { Container } from '@mui/material';
 import { useTranslation } from 'react-i18next';
 import Link from 'next/link';
 
@@ -16,17 +16,6 @@ export default function AdminLayout({ children }: { children: React.ReactNode })
 
   return (
     <>
-      <AppBar position="static" color="primary">
-          <LinkComponent href="/admin/roles">
-            <Button color="inherit">{t('admin.roles')}</Button>
-          </LinkComponent>
-          <LinkComponent href="/admin/statuses">
-            <Button color="inherit">{t('admin.statuses')}</Button>
-          </LinkComponent>
-          <LinkComponent href="/admin/users">
-            <Button color="inherit">{t('admin.users')}</Button>
-          </LinkComponent>
-      </AppBar>
       <Container maxWidth="lg" sx={{ mt: 4 }}>
         {children}
       </Container>
